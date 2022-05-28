b871ae98360dda378566ae0d13532f30944051f5
JunKangDeloitte <junkwong@deloitte.com>
2022-05-28 11:22:08 +0800

    Completed Health Check Page and Tabs Switching features

---
+++ js/loadTasklistDetailsTabs.js
@@ -0,0 +1,9 @@
+$("#partial").load("./shared_health_check.html");
+
+function loadHealthCheck() {
+  $("#partial").load("./shared_health_check.html");
+}
+
+function loadMatching() {
+  $("#partial").load("./shared_matching.html");
+}
